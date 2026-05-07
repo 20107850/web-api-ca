692742ae1596d4ab9cd3930823d89021f6013ed8
ice <20107850@mail.wit.ie>
2026-05-08 00:20:42 +0100

    getMovies now using our Movies API

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -1,11 +1,26 @@
 import express from 'express';
 import asyncHandler from 'express-async-handler';
-import { getMovies } from '../tmdb-api'; 
+import { getMovies, getUpcomingMovies, getGenres, getMovie } from '../tmdb-api'; 
 
 const router = express.Router();
 router.get('/discover', asyncHandler(async (req, res) => {
     const discoverMovies = await getMovies();
     res.status(200).json(discoverMovies);
+}));
+
+router.get('/upcoming', asyncHandler(async (req, res) => {
+    const upcomingMovies = await getUpcomingMovies();
+    res.status(200).json(upcomingMovies);
+}));
+
+router.get('/genres', asyncHandler(async (req, res) => {
+    const genres = await getGenres();
+    res.status(200).json(genres);
+}));
+
+router.get('/:id', asyncHandler(async (req, res) => {
+    const movie = await getMovie(req.params.id);
+    res.status(200).json(movie);
 }));
 
 // movie routes to be added
