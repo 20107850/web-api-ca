2185546e3c3322b5f71573be2b247ecd89f2112f
ice <20107850@mail.wit.ie>
2026-05-08 13:52:55 +0100

    getMovies now using our Movies API

---
+++ react-movies/movies/src/contexts/authContext.jsx
@@ -0,0 +1,54 @@
+import { login, signup } from "../api/tmdb-api";
+
+import { useState, createContext } from "react";
+import { login, signup } from "../api/tmdb-api";
+
+export const AuthContext = createContext(null); //eslint-disable-line
+
+const AuthContextProvider = (props) => {
+  const existingToken = localStorage.getItem("token");
+  const [isAuthenticated, setIsAuthenticated] = useState(existingToken ? true : false);
+  const [authToken, setAuthToken] = useState(existingToken); //eslint-disable-line
+  const [userName, setUserName] = useState("");
+
+  const setToken = (data) => {
+    localStorage.setItem("token", data);
+    setAuthToken(data);
+  }
+
+  const authenticate = async (username, password) => {
+    const result = await login(username, password);
+
+    if (result.token) {
+      setToken(result.token);
+      setIsAuthenticated(true);
+      setUserName(username);
+    }
+  };
+
+  const register = async (username, password) => {
+    const result = await signup(username, password);
+    return result.success;
+  };
+
+  const signout = () => {
+    localStorage.removeItem("token");
+    setTimeout(() => setIsAuthenticated(false), 100);
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isAuthenticated,
+        authenticate,
+        register,
+        signout,
+        userName
+      }}
+    >
+      {props.children} {/* eslint-disable-line */}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContextProvider;
