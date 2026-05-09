d1ada57a2709bbca5a94363dd874c3fb3418d63e
ice <20107850@mail.wit.ie>
2026-05-09 01:50:30 +0100

    Added authentication routes to movies app

--- react-movies/movies/src/contexts/authContext.jsx
+++ react-movies/movies/src/contexts/authContext.jsx
@@ -1,7 +1,6 @@
-import { login, signup } from "../api/tmdb-api";
-
 import { useState, createContext } from "react";
 import { login, signup } from "../api/tmdb-api";
+
 
 export const AuthContext = createContext(null); //eslint-disable-line
 
