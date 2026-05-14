b1f69276b76b53d583ab619ce0e470375fd2f8de
ice <20107850@mail.wit.ie>
2026-05-14 19:52:44 +0100

    Added user watchlist route

--- movies-api/api/watchlist/index.js
+++ movies-api/api/watchlist/index.js
@@ -9,4 +9,9 @@ router.get('/', asyncHandler(async (req, res) => {
     res.status(200).json(watchlist);
 }));
 
+router.get('/user/:username', asyncHandler(async (req, res) => {
+    const watchlist = await Watchlist.find({ username: req.params });
+    res.status(200).json(watchlist);
+}));
+
 export default router;
