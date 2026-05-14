cad1a43896164ecb1241d971f3adfd64c124fd24
ice <20107850@mail.wit.ie>
2026-05-14 19:58:13 +0100

    Added user watchlist route

--- movies-api/api/watchlist/index.js
+++ movies-api/api/watchlist/index.js
@@ -10,7 +10,7 @@ router.get('/', asyncHandler(async (req, res) => {
 }));
 
 router.get('/user/:username', asyncHandler(async (req, res) => {
-    const watchlist = await Watchlist.find({ username: req.params });
+    const watchlist = await Watchlist.find({ username: req.params.username });
     res.status(200).json(watchlist);
 }));
 
